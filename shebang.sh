5292002f76b4a4fc3357a558bd58df3a32287038
gajendra singh <gsingh.dba@gmail.com>
2023-03-17 23:51:33 +0530

    removed keypair from command

--- shebang.sh
+++ shebang.sh
@@ -11,6 +11,6 @@ echo "$(who | wc -l)"
 echo "testing"
 echo -e "\e[32m I am in \e[34m color"
 
-aws ec2 run-instances --image-id ami-05afd67c4a44cc983 --count 1 --instance-type t2.micro --key-name abc  --security-group-ids sg-04d6f37e1d3fd1d34 --region ap-south-1 | jq '.Instances[].InstanceId'
+#aws ec2 run-instances --image-id ami-05afd67c4a44cc983 --count 1 --instance-type t2.micro --key-name abc  --security-group-ids sg-04d6f37e1d3fd1d34 --region ap-south-1 | jq '.Instances[].InstanceId'
 #aws ec2 describe-instances --region ap-south-1 | jq '.Instances[].InstanceId'
 aws s3 ls
