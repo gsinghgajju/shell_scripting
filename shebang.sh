643efc80617a314c23272d4f51f237c62d46b6cd
gajendra singh <gsingh.dba@gmail.com>
2023-03-17 15:25:25 +0530

    removed keypair from command

--- shebang.sh
+++ shebang.sh
@@ -11,4 +11,4 @@ echo "$(who | wc -l)"
 echo "testing"
 echo -e "\e[32m I am in \e[34m color"
 
-aws ec2 run-instances --image-id ami-05afd67c4a44cc983 --count 1 --instance-type t2.micro --key-name abc  --security-group-ids sg-04d6f37e1d3fd1d34
+aws ec2 run-instances --image-id ami-05afd67c4a44cc983 --count 1 --instance-type t2.micro --key-name abc  --security-group-ids sg-04d6f37e1d3fd1d34 --region ap-south-1
